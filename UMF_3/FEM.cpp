5460d509a24bf3dc5f69b1ba3dab69f30f397e79
AlexandraGribova <sasha.gribova@bk.ru>
2022-05-02 15:24:16 +0700

    generate_portrait

--- UMF_3/FEM.cpp
+++ UMF_3/FEM.cpp
@@ -133,23 +133,33 @@ private:
         double f1, f2, f3, f4;
         double g;
         int wi;
+        int l;
         for (int j = 0; j < ny - 1; j++)
         {
             y2 = grid.getY(j + 1);
             y1 = grid.getY(j);
             hy = y2 - y1;
             for (int i = 0; i < nx - 1; i++) {
+                l = j * nx + i;
                 x2 = grid.getX(i + 1);
                 x1 = grid.getX(i);
                 hx = x2 - x1;
-                f1 = grid.f(wi, x1, y1);
-                f2 = grid.f(wi, x2, y1);
-                f3 = grid.f(wi, x1, y2);
-                f4 = grid.f(wi, x2, y2);
                 wi = grid.inSubArea(i, j);
+                if (!(l % 2)) {  // если чётное
+                    f1 = grid.fc(wi, x1, y1);
+                    f2 = grid.fc(wi, x2, y1);
+                    f3 = grid.fc(wi, x1, y2);
+                    f4 = grid.fc(wi, x2, y2);
+                }
+                else {
+                    f1 = grid.fs(wi, x1, y1);
+                    f2 = grid.fs(wi, x2, y1);
+                    f3 = grid.fs(wi, x1, y2);
+                    f4 = grid.fs(wi, x2, y2);
+                }
                 g = hx * hy;
                 for (int i = 0; i < 4; i++)
-                    b[i] += g * (C[i][0] * f1 + C[i][1] * f2 + C[i][2] * f3 + C[i][3] * f4);
+                    b[l] += g * (C[i][0] * f1 + C[i][1] * f2 + C[i][2] * f3 + C[i][3] * f4);
             }
         }
     }
@@ -157,15 +167,7 @@ private:
    
 
     void boundary_condition(Grid& grid, double t2) {
-        double u1_ = u1(t2);
-        double u2_ = u2(t2);
-        r[0] = 0;
-        d[0] = 1;
-        b[0] = u1_;
-
-        l[n - 2] = 0;
-        d[n - 1] = 1;
-        b[n - 1] = u2_;
+        
     }
 
 
@@ -174,18 +176,5 @@ private:
         for (int i = 0; i < n; i++)
             norm += v[i] * v[i];
         return sqrt(norm);
-    }
-
-    // ÆÓÒÊÎ ÍÅÝÔÔÅÊÒÈÂÍÎ
-    void update_matrix(Grid& grid, double t1, double t2) {
-        for (int i = 0; i < n - 1; i++) { // êîñòûëü
-            d[i] = 0;
-            l[i] = 0;
-            r[i] = 0;
-        }
-        d[n - 1] = 0;
-        M_matrix_assembly(grid, t1, t2); // êîñòûëü
-        G_matrix_assembly(grid, t2);
-        boundary_condition(grid, t2);  // êîñòûëü
     }
 };
